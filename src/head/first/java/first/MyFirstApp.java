89c7e8f05b3a4a6e080b6160e5d976208e2e9d5c
yudaev.a@me.com <Ntktajy89>
2017-03-29 11:40:17 +0300

    1

---
+++ src/head/first/java/first/MyFirstApp.java
@@ -0,0 +1,21 @@
+/*
+*Создание класса с методом main (39)
+ */
+package head.first.java.first;
+
+public class MyFirstApp {
+
+    public static void main(String[] args) {
+        System.out.println("Я управляю");
+        System.out.println("Миром");
+
+        int z = 9;
+        while (z >= 9 && z <= 20) {
+
+            System.out.println(z);
+            z++;
+
+        }
+    }
+
+}
